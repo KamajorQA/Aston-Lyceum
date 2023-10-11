9a2cc455065a01790b5de9f6baae568e2c4874a0
Kamajor <kamajorqa@gmail.com>
2023-10-11 20:08:26 +0300

    Замена сборщика Turbopack на Webpack по-умолчанию для устранения ошибок импортов и hot-reload'а. Добавлены socials в подвал.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,32 @@
+import telegram from '@/public/icons/telegram.svg';
+import gitHub from '@/public/icons/gitHub.svg';
+import linkedIn from '../../public/icons/linkedIn.svg';
+import { SocialItem } from '../SocialItem/SocialItem';
 import s from './footer.module.css';
 
 function Footer() {
   return (
     <footer className={`container ${s.footer}`}>
-      Created by &copy; Kamajor {new Date().getFullYear()}
+      <div className={s.copyright}>
+        Created by &copy; Kamajor {new Date().getFullYear()}
+      </div>
+      <ul className={s.socials}>
+        <SocialItem
+          href={'https://t.me/KamajorQA'}
+          imgSrc={telegram}
+          alt="Telegram Link"
+        />
+        <SocialItem
+          href={'https://github.com/KamajorQA'}
+          imgSrc={gitHub}
+          alt="GitHub Link"
+        />
+        <SocialItem
+          href={'https://www.linkedin.com/in/KamajorQA'}
+          imgSrc={linkedIn}
+          alt="LinkedIn Link"
+        />
+      </ul>
     </footer>
   );
 }
---
+++ components/SocialItem/SocialItem.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import s from './socialItem.module.css';
+
+interface ISocialItemProps {
+  href: string;
+  imgSrc: string;
+  alt: string;
+}
+
+function SocialItem({ href, imgSrc, alt }: ISocialItemProps) {
+  return (
+    <li>
+      <a href={href} target="_blank" rel="noreferrer">
+        <Image src={imgSrc} alt={alt} className={s.socialIcon} height={25} />
+      </a>
+    </li>
+  );
+}
+
+export { SocialItem };
