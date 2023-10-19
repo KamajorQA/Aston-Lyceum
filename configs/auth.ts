f7461c432f03b70a10cac812fe8d84148678ed66
Kamajor <kamajorqa@gmail.com>
2023-10-19 23:54:09 +0300

    Добавлена аутентификация через провайдера Github. Добавлена конфигурация для OAuth провайдера Github.

--- configs/auth.ts
+++ configs/auth.ts
@@ -1,11 +1,16 @@
 import { AuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
 
 const authConfig: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
 };
