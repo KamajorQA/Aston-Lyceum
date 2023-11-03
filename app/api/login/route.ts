a44135cf18b7f49f98eb0f6f0ca2e31ed5c7ac25
Kamajor <kamajorqa@gmail.com>
2023-11-03 22:19:57 +0300

    Создан API роут для Логина. Добавлена библиотека для хеширования bcrypt.

---
+++ app/api/login/route.ts
@@ -0,0 +1,19 @@
+import prisma from '@/data/prisma';
+
+interface RequestBody {
+  email: string;
+  password: string;
+}
+
+export async function POST(request: Request) {
+  const body: RequestBody = await request.json();
+
+  const user = await prisma.user.findFirst({
+    where: {
+      email: body.email,
+    },
+  });
+
+  if (user && user.password === body.password) {
+  }
+}
