286b5fd55a87f4bfbca1318929d7387a9df8f78d
Kamajor <kamajorqa@gmail.com>
2023-12-06 08:10:46 +0300

    Изменен запрос к БД через Призму для получения конкретной инструкции по id - исключен повторный запрос всех записей из БД с последующим локальным поиском, заменено на прямой поиск в БД по заданному параметру и сразу возврат конкретного значения.

--- app/api/instructions/[id]/route.ts
+++ app/api/instructions/[id]/route.ts
@@ -9,11 +9,11 @@ interface IParams {
 export async function GET(request: Request, { params }: IParams) {
   const instructionId = +params.id;
 
-  const instructionsList = await prisma.post.findMany();
-
-  const currentInstruction = instructionsList.find(
-    (post) => post.id === instructionId
-  );
+  const currentInstruction = await prisma.post.findUnique({
+    where: {
+      id: instructionId,
+    },
+  });
 
   return NextResponse.json(currentInstruction);
 }
